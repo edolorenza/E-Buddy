17d8a9408ae87f32b8d4d18ddfc2e89523ae3dfe
edolorenza <edolrnz@gmail.com>
2024-12-22 20:31:07 +0700

    initial commit

---
+++ E-Buddy/E_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_BuddyApp.swift
+//  E-Buddy
+//
+//  Created by Edo Lorenza on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct E_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
